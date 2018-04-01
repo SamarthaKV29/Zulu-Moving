80c77e5273b75542b9c3ea582a8e6dbf241efc06
SamarthaKV29 <samarthakv29@gmail.com>
2018-04-01 15:36:10 -0400

    updxx

--- index.php
+++ index.php
@@ -344,7 +344,7 @@
 
 								<div class="row">
 									<div class="col-lg-12" style="display:block; font-family: &quot;Kelly Slab&quot;">
-										
+										<?php require("dbconnect.php") ?>
 									</div>
 								</div>
 							</div>
