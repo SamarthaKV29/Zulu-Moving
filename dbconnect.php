839e7fba8962cb32944657b708ab2fe6d825126c
SamarthaKV29 <samarthakv29@gmail.com>
2018-04-01 19:45:48 -0400

    updxx

--- dbconnect.php
+++ dbconnect.php
@@ -7,10 +7,10 @@ function pg_connection_string_from_database_url() {
 try{
     $pg_conn = pg_connect(pg_connection_string_from_database_url());
     $result = pg_query($pg_conn, "SELECT * FROM testimonials;");
-    echo "<div class='row'><div class='col-md-6 center'><ul>";
+    echo "<div class='row'><div class='col-md-6 center-block'><ul>";
     while ($row = pg_fetch_assoc($result)) {
-        echo "<li class='text-success'>";
-        echo "<div class='panel panel-default'><div class='panel-heading'>"."User:".$row["emailid"]."</div>";
+        echo "<li class='bg-info'>";
+        echo "<div class='panel panel-info'><div class='panel-heading'>"."User:".$row["emailid"]."</div>";
         echo "<div class='panel-body'>".$row["review"]."</div>";
         echo "</li>";
     }
