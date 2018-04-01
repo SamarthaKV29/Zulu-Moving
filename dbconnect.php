6927ebd4d6d4b5f3bf704a02d1cc382af15840b0
SamarthaKV29 <samarthakv29@gmail.com>
2018-04-01 18:18:05 -0400

    updxx

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,14 @@
-<?php 
+<?php
+function pg_connection_string_from_database_url() {
+    extract(parse_url($_ENV["DATABASE_URL"]));
+    return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+  }
+ 
 try{
-    $dbopts = parse_url(getenv('DATABASE_URL'));
-    echo $dbopts;
+    $pg_conn = pg_connect(pg_connection_string_from_database_url());
+    echo "Access";
 }
 catch(Exception $e){
-    echo "Except";
+    echo "Unable to access our Database.";
 }
 ?>
