b807b4d0163b3ad722e51010f20158e2c5b1d069
SamarthaKV29 <samarthakv29@gmail.com>
2018-04-01 19:39:52 -0400

    updxx

--- dbconnect.php
+++ dbconnect.php
@@ -10,8 +10,8 @@ try{
     echo "<div class='row'><div class='col-md-6 center'><ul>";
     while ($row = pg_fetch_assoc($result)) {
         echo "<li class='text-success'>";
-        echo "User:".$row["emailid"]."<br>";
-        echo $row["review"];
+        echo "<div class='panel panel default'><div class='panel-heading'>"."User:".$row["emailid"]."</div>";
+        echo "<div class='panel-body'>".$row["review"]."</div>";
         echo "</li>";
     }
     echo "</ul></div></div><hr>";
